d2e4ab555d98951c495272dc7a0c5cd384ede589
Viacheslav Kaliak <trinidaa@gmial.com>
2024-12-17 12:25:59 +0100

    Solution

---
+++ INSTRUCTION.md
@@ -0,0 +1,12 @@
+## HOW shoude TO USE
+
+### -- 1. Oper mysql client  
+mysql -u your_username -p  
+
+### -- 2. Enter passwor: (from base owner)
+
+### -- 3. Select database.  
+USE ShopDB;  
+
+### -- 4. Call process transaction. 
+ CALL CreateOrder();  
--- task.sql
+++ task.sql
@@ -1,11 +1,24 @@
--- Use our database
-USE ShopDB; 
+DELIMITER $$
+USE ShopDB;
+CREATE PROCEDURE CreateOrder()
+BEGIN
 
--- Some data should be created outside the transaction (here)
+    DECLARE last_order_id INT;
+    DECLARE warehouse_amount INT;
+    INSERT INTO Orders (CustomerID, Date) VALUES (1, '2023-01-02');
+    SET last_order_id = LAST_INSERT_ID();
+    START TRANSACTION;
+    SET warehouse_amount = (SELECT WarehouseAmount FROM Products WHERE ID = 1);
+    IF warehouse_amount >= 1 THEN
+        INSERT INTO OrderItems (OrderID, ProductID, Count)
+        VALUES (@last_order_id, 1, 1);
+        UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
+        COMMIT;
+    ELSE
+        ROLLBACK;
+        SELECT 'No more products' AS ErrorMessage;
+    END IF;
 
--- Start the transaction 
-START TRANSACTION; 
+END $$
 
--- And some data should be created inside the transaction 
-
-COMMIT; 
+DELIMITER ;
